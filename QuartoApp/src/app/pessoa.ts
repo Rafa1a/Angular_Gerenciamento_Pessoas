7763822b4b4c9dac64cde59385d74760efad4945
Rafa1a <110539932+Rafa1a@users.noreply.github.com>
2023-03-08 00:22:34 -0300

    up project
    adicionando

---
+++ QuartoApp/src/app/pessoa.ts
@@ -0,0 +1,11 @@
+export class Pessoa {
+    nome:string;
+    cpf:string;
+    idade:number;
+    constructor(nome:string="", cpf:string="",idade:number=0){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.idade = idade;
+    }
+   }
+   
